73a590ebea5d42bcb73527f4e880e1f487206592
AlbertBagatelas <maxbagatelas@hotmail.com>
2021-07-11 16:29:00 +0000

    added javascript code to submit the difficulty form and place the player in the correct difficulty quiz and their username for the session in hand

---
+++ assets/javascript/script.js
@@ -0,0 +1,30 @@
+let usernames = [];
+let form = document.getElementById('form');
+form.addEventListener('submit', difficultyForm);
+let errorMsg = document.getElementById('error-msg');
+
+function difficultyForm(event){
+    event.preventDefault();
+    
+    let username = document.getElementById('username').value;
+    if (usernames.includes(username)) {
+        errorMsg.innerHTML = `Sorry, the username ${username} is already in use.`;
+    } else {
+        usernames.push(username);
+        console.log(usernames);
+    }
+
+    let difficulty = document.getElementsByName('difficulty').value;
+    if (difficulty !== 'Apprentice') {
+        form.submit();
+        window.location.replace();
+    } if (difficulty !== 'Sith Lord') {
+        form.submit();
+        window.location.replace();
+    } if (difficulty !== 'Dark Lord') {
+        form.submit();
+        window.location.replace();
+    } else {
+        errorMsg.innerHTML = `Please select a difficulty!`
+    }
+}
